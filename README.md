fbcc949eb59312f32d287ea32841f8593a4088b3
samadii <77415087+samadii@users.noreply.github.com>
2021-09-22 22:45:39 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# OCR Bot
+
+A Telegram bot to extracting text from images. All languages supported.
+
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/samadii/OCR-Bot)
+
+
+### Devs: 
+- [@samadii](https://github.com/samadii)
---
+++ bot.py
@@ -0,0 +1,78 @@
+import os
+import pytesseract
+import requests
+from PIL import Image
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import MessageEmpty
+from pyromod import listen
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+
+Bot = Client(
+    "OCRBot",
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
+)
+
+START_TXT = """
+Hi {}
+I am OCR Bot.
+
+> `I can extract any text from images using OCR technology. All languages supported.`
+
+Send an image to get started.
+"""
+
+START_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Source Code', url='https://github.com/samadii/OCR-Bot'),
+        ]]
+    )
+
+
+@Bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+
+@Bot.on_message(filters.private & filters.photo)
+async def ocr(bot, msg):
+    lang_code = await bot.ask(msg.chat.id,'`Now send the ISO language code.`\n\n[List of ISO 639-2 language codes](https://en.m.wikipedia.org/wiki/List_of_ISO_639-2_codes)', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
+    data_url = f"https://github.com/tesseract-ocr/tessdata/raw/main/{lang_code.text}.traineddata"
+    dirs = '/app/vendor/tessdata/'
+    path = f'{dirs}{lang_code.text}.traineddata'
+    if not os.path.exists(path):
+        data = requests.get(data_url, allow_redirects=True, headers={'User-Agent': 'Mozilla/5.0'})
+        if data.status_code == 200:
+            open(path, 'wb').write(data.content)
+        else:
+            return await msg.reply("`Either the lang code is wrong or the lang is not supported.`", parse_mode='md')
+    user_id = msg.from_user.id
+    message_id = msg.message_id
+    name_format = f"text_{user_id}_{message_id}"
+    message = await msg.reply("`Downloading and Extracting...`", parse_mode='md')
+    image = await msg.download(file_name=f"{name_format}.jpg")
+    img = Image.open(image)
+    text = pytesseract.image_to_string(img, lang=f"{lang_code.text}")
+    text = text[:-1]
+    try:
+        await msg.reply(text, quote=True, disable_web_page_preview=True)
+    except MessageEmpty:
+        return await msg.reply("`Either the image has no text or the text is not recognizable.`", quote=True, parse_mode='md')
+    await message.delete()
+    os.remove(image)
+
+
+    
+Bot.run()
